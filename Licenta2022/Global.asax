ae494c44525ad543d2a0ffdd574c9a8593c39cb6
dariahazaparu <61266022+dariahazaparu@users.noreply.github.com>
2022-03-29 02:40:52 +0300

    Add project files.

---
+++ Licenta2022/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Licenta2022.MvcApplication" Language="C#" %>
---
+++ Licenta2022/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Licenta2022.Startup))]
+namespace Licenta2022
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
