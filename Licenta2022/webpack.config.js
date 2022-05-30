a0b7af45169dcf6d6074dae20db19ffda531ddf8
qfl1ck32 <rusuandreicristian32@gmail.com>
2022-05-30 03:30:29 +0300

    Add loader for css

--- Licenta2022/webpack.config.js
+++ Licenta2022/webpack.config.js
@@ -1,59 +1,67 @@
-const path = require('path');
-const ManifestPlugin = require('webpack-manifest-plugin');
+const path = require("path");
+const ManifestPlugin = require("webpack-manifest-plugin");
 
 module.exports = {
-	entry: './Content/components/expose-components.js',
-	output: {
-		filename: '[name].js',
-		globalObject: 'this',
-		path: path.resolve(__dirname, 'dist/'),
-		publicPath: '/dist/'
-	},
-	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-	optimization: {
-		runtimeChunk: {
-			name: 'runtime', // necessary when using multiple entrypoints on the same page
-		},
-		splitChunks: {
-			cacheGroups: {
-				commons: {
-					test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-					name: 'vendor',
-					chunks: 'all',
-				},
-			},
-		},
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-			},
-			{
-				test: /\.tsx?$/,
-				exclude: /node_modules/,
-				loader: 'ts-loader'
-			}
-		],
-	},
-	plugins: [
-		new ManifestPlugin({
-			fileName: 'asset-manifest.json',
-			generate: (seed, files) => {
-				const manifestFiles = files.reduce((manifest, file) => {
-					manifest[file.name] = file.path;
-					return manifest;
-				}, seed);
-
-				const entrypointFiles = files.filter(x => x.isInitial && !x.name.endsWith('.map')).map(x => x.path);
-
-				return {
-					files: manifestFiles,
-					entrypoints: entrypointFiles,
-				};
-			},
-		}),
-	]
+  entry: "./Content/components/expose-components.js",
+  output: {
+    filename: "[name].js",
+    globalObject: "this",
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+  },
+  resolve: {
+    fallback: {
+      buffer: false,
+      stream: false,
+    },
+  },
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  optimization: {
+    runtimeChunk: {
+      name: "runtime", // necessary when using multiple entrypoints on the same page
+    },
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+          name: "vendor",
+          chunks: "all",
+        },
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    // new ManifestPlugin({
+    // 	fileName: 'asset-manifest.json',
+    // 	generate: (seed, files) => {
+    // 		const manifestFiles = files.reduce((manifest, file) => {
+    // 			manifest[file.name] = file.path;
+    // 			return manifest;
+    // 		}, seed);
+    // 		const entrypointFiles = files.filter(x => x.isInitial && !x.name.endsWith('.map')).map(x => x.path);
+    // 		return {
+    // 			files: manifestFiles,
+    // 			entrypoints: entrypointFiles,
+    // 		};
+    // 	},
+    // }),
+  ],
 };
